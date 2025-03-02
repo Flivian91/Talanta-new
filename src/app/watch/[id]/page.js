0135d0db02611c4daeaa3586571af59874675b04
Flivian91 <fliviandev@gmail.com>
2025-03-02 12:44:35 +0300

    commit

--- src/app/watch/[id]/page.js
+++ src/app/watch/[id]/page.js
@@ -9,13 +9,17 @@ function WatchPage() {
   return (
     <div className="flex flex-col gap-4 md:px-2 md:py-4">
       <div className="relative grid gap-4 grid-cols-1 md:grid-cols-[3fr_350px]">
-        <VideoPlayer />
+        <div className="flex flex-col gap-2">
+          <VideoPlayer />
+          <VideoInfo />
+          <VideoActions />
+        </div>
+
         <div className="hidden md:block">
           <CommentSection />
         </div>
       </div>
-      <VideoInfo />
-      <VideoActions />
+
       <div className="block md:hidden">
         <CommentSection />
       </div>
--- src/components/video/VideoInfo.jsx
+++ src/components/video/VideoInfo.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function VideoInfo() {
-  return <div>Video Information</div>;
+  return (
+    <div className="px-2 py-2">
+      <div className="flex items-center gap-2 justify-between">
+        <h1>Video Description Here</h1>
+        <p>27/3/2025</p>
+      </div>
+    </div>
+  );
 }
 
 export default VideoInfo;
